c7c5b68ba997aec8432d9b4d9421fe8e93f49719
Jordan Hancock <s11719947@gg0707i1-dm0006.staff.uce.ac.uk>
2013-11-18 14:39:22 +0000

    Initial Commit

---
+++ iFindAJob/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iFindAJob
+//
+//  Created by Jordan Hancock on 18/11/2013.
+//  Copyright (c) 2013 Jordan Hancock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iFindAJob/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iFindAJob
+//
+//  Created by Jordan Hancock on 18/11/2013.
+//  Copyright (c) 2013 Jordan Hancock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
